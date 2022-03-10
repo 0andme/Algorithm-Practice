197a158273400bcd97e7710fe5d029bc875e33fc
0andme <yu00mi97@gmail.com>
2022-03-11 01:20:58 +0900

    feat : 1001 a - b

---
+++ solutions/단계별/1단계/1001.js
@@ -0,0 +1,7 @@
+var fs = require("fs");
+var input = fs.readFileSync("./data.txt").toString().split(" ");
+// var input = fs.readFileSync('/dev/stdin').toString().split(' ');
+const dataA = Number(input[0]);
+const dataB = Number(input[1]);
+
+console.log(dataA - dataB);
