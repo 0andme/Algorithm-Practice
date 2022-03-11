f6acebad2c0af1ecbdfe12730e30452c9f752c7e
0andme <yu00mi97@gmail.com>
2022-03-12 03:52:31 +0900

    feat : 9498 시험 성적

---
+++ solutions/단계별/1단계/9498.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+let input = fs.readFileSync("./data.txt").toString();
+// let input=fs.readFileSync('/dev/stdin').toString()
+input = Number(input);
+if (input > 89) console.log("A");
+else if (input > 79) console.log("B");
+else if (input > 69) console.log("C");
+else if (input > 59) console.log("D");
+else console.log("F");
