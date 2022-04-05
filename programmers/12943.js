3a341ecee851bb50709e97981b6fbdf2f1604e21
0andme <yu00mi97@gmail.com>
2022-04-06 01:02:29 +0900

    12943 콜라츠 추측

---
+++ programmers/12943.js
@@ -0,0 +1,18 @@
+function solution(num) {
+  let count = 0;
+  let res = 0;
+  while (num !== 1) {
+    if (num % 2 === 0) {
+      num = num / 2;
+      count += 1;
+    } else {
+      num = num * 3 + 1;
+      count += 1;
+    }
+    if (count === 500) {
+      count = -1;
+      break;
+    }
+  }
+  return count;
+}
