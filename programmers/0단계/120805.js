cab5efb93f3db55be56fc67f0672245c29daac9e
0andme <yu00mi97@gmail.com>
2022-10-04 14:48:18 +0900

    120805 몫 구하기

---
+++ programmers/0단계/120805.js
@@ -0,0 +1,7 @@
+// 120805 몫 구하기
+// 정수 num1, num2가 매개변수로 주어질 때
+// num1을 num2로 나눈 몫을 return 하도록 solution 함수를 완성해주세요.
+function solution(num1, num2) {
+  let answer = parseInt(num1 / num2);
+  return answer;
+}
