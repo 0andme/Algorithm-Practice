384e5f04e06fa8e90df95565c8970634f817923e
0andme <yu00mi97@gmail.com>
2022-10-26 23:54:27 +0900

    120895 인덱스 바꾸기

---
+++ programmers/0단계/120895.js
@@ -0,0 +1,13 @@
+// 120895 인덱스 바꾸기
+// 문자열 my_string과 정수 num1, num2가 매개변수로 주어질 때,
+// my_string에서 인덱스 num1과 인덱스 num2에 해당하는 문자를 바꾼 문자열을 return
+function solution(my_string, num1, num2) {
+  let answer = "";
+  let strArr = my_string.split("");
+  let temp = strArr[num1];
+  strArr[num1] = strArr[num2];
+  strArr[num2] = temp;
+
+  answer = strArr.join("");
+  return answer;
+}
