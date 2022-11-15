31f54f4df4d82070a672321c6514caead4f7a15d
0andme <yu00mi97@gmail.com>
2022-11-16 03:27:30 +0900

    10816 숫자 카드 2

---
+++ solutions/정렬/10816.js
@@ -0,0 +1,23 @@
+// 10816 숫자 카드 2
+const fs = require("fs");
+const input = fs.readFileSync("./10816.txt").toString().trim().split("\n");
+const N = input[0];
+const nums = input[1].split(" ");
+const M = input[2];
+const targetN = input[3].split(" ");
+
+const map = new Map();
+for (let i = 0; i < N; i++) {
+  if (map.has(nums[i])) {
+    map.set(nums[i], map.get(nums[i]) + 1);
+  } else {
+    map.set(nums[i], 1);
+  }
+}
+let answer = [];
+for (let i = 0; i < M; i++) {
+  const n = map.has(targetN[i]) ? map.get(targetN[i]) : 0;
+  answer.push(n);
+}
+
+console.log(answer.join(" "));
