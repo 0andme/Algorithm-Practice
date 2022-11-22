8dd8fbe02d8a2533a1e9720202d2101933b19e4b
0andme <yu00mi97@gmail.com>
2022-11-22 20:13:24 +0900

    2798 블랙잭

---
+++ lecture/자료구조풀이/2798블랙잭.js
@@ -0,0 +1,23 @@
+// 2798 블랙잭
+
+// 삼중 폴문으로 구현
+
+const fs = require("fs");
+const input = fs.readFileSync("./2798.txt").toString().trim().split("\n");
+
+const [N, M] = input[0].split(" ");
+const data = input[1].split(" ").map(Number);
+let max = 0;
+
+for (let i = 0; i < N; i++) {
+  for (let j = i + 1; j < N; j++) {
+    for (let k = j + 1; k < N; k++) {
+      const sum = data[i] + data[j] + data[k];
+      if (sum <= M) {
+        max = Math.max(max, sum);
+      }
+    }
+  }
+}
+
+console.log(max);
